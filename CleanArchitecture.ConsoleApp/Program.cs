55d90a78b880c270e7a59ad6ad3e7c727a1eebc6
Arthur Zimmermann <arthur.zimmermann11@gmail.com>
2024-12-17 16:57:04 -0300

    Adicionar arquivos de projeto.

---
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchitecture.Data;
+using CleanArchitecture.Domain;
+
+StreamerDbContext context = new();
+
+
+void QueryStreaming()
+{
+	var streamers = context!.Streamers!.ToList();
+	foreach (var streamer in streamers)
+	{
+		Console.WriteLine($"Streamer: {streamer.Id}-{streamer.Name}");
+	}
+}
+
+async Task AddNewRecords()
+{
+	Streamer streamer = new()
+	{
+		Name = "disney",
+		Url = "https://www.disney.com"
+	};
+
+	context!.Streamers!.Add(streamer);
+	await context.SaveChangesAsync();
+
+	var movies = new List<Video>
+	{
+		new() { Name = "Casa de papel", StreamerId = streamer.Id },
+		new() { Name = "Homem Aranha", StreamerId = streamer.Id },
+		new() { Name = "Vingadores", StreamerId = streamer.Id },
+		new() { Name = "Homem de ferro", StreamerId = streamer.Id }
+	};
+	await context.AddRangeAsync(movies);
+	await context.SaveChangesAsync();
+}
---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Data
+{
+	public class StreamerDbContext : DbContext
+	{
+        public StreamerDbContext(DbContextOptions<StreamerDbContext> options) : base(options)
+        {
+            
+        }
+        public StreamerDbContext()
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlServer("Data Source=DESKTOP-D19QQ33;Persist Security Info=True;User ID=arthur;Password=arthur;Encrypt=False")
+				.LogTo(Console.WriteLine, new[] {DbLoggerCategory.Database.Command.Name},LogLevel.Information)
+				.EnableSensitiveDataLogging()
+				.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+		}
+		public DbSet<Streamer>? Streamers { get; set; }
+		public DbSet<Video>? Videos{ get; set; }
+    }
+}
