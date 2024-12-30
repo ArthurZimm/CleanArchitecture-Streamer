0f1a185614fb29ba884078ea4422896eac33546e
Arthur Zimmermann <arthur.zimmermann11@gmail.com>
2024-12-30 12:10:29 -0300

    Adicionar arquivos de projeto.

---
+++ CleanArchitecture.Infrastructure/Persistence/StreamerDbContext.cs
@@ -0,0 +1,65 @@
+﻿using CleanArchitecture.Domain;
+using CleanArchitecture.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public class StreamerDbContext : DbContext
+    {
+        public StreamerDbContext(DbContextOptions<StreamerDbContext> options) : base(options)
+        {
+
+        }
+        public StreamerDbContext()
+        {
+
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //    optionsBuilder.UseSqlServer("Data Source=DESKTOP-D19QQ33;Persist Security Info=True;User ID=arthur;Password=arthur;Encrypt=False")
+        //        .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+        //        .EnableSensitiveDataLogging()
+        //        .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        //}
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "system";
+                        break;
+
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "system";
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Streamer>()
+                .HasMany(s => s.Videos)
+                .WithOne(v => v.Streamer)
+                .HasForeignKey(v => v.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Video>()
+                .HasMany(v => v.Actors)
+                .WithMany(a => a.Videos)
+                .UsingEntity<VideoActor>
+                (pt => pt.HasKey(e => new { e.VideoId, e.ActorId })
+                );
+        }
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+    }
+}
---
+++ CleanArchitecture.Infrastructure/Persistence/StreamerDbContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public class StreamerDbContextSeed
+    {
+        public static async Task SeedAsync(StreamerDbContext context, ILogger<StreamerDbContextSeed>logger)
+        {
+            if(!context.Streamers!.Any())
+            {
+                context.Streamers!.AddRange(GetPreConfiguredStreamer());
+                await context.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(StreamerDbContext).Name);
+            }
+        }
+
+        private static IEnumerable<Streamer> GetPreConfiguredStreamer()
+        {
+            return new List<Streamer>
+            {
+                new Streamer
+                {
+                    CreatedBy = "Arthur", Name = "Maxi HBP", Url = "https://www.hbo.com"
+                },
+                new Streamer
+                {
+                    CreatedBy = "Arthur", Name = "Amazon", Url = "https://www.amazon.com"
+                }
+        };
+        }
+    }
+}
